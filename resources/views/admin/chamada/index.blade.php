7db30c3f65dc76ed81ecd1478a8fe00390a3d311
João Moita Manrique <joaomoita@pacto.goiania>
2018-09-18 09:01:45 -0300

    Adicionado o butao excluir na listagem de index

--- resources/views/admin/chamada/index.blade.php
+++ resources/views/admin/chamada/index.blade.php
@@ -8,7 +8,9 @@
         <thead>
         <tr>
             <th>#</th>
-            <th>title</th>
+            <th>Data e hora da ligação</th>
+            <th>Número de origem</th>
+            <th>Número de destino</th>
             <th class="text-right">ações</th>
         </tr>
         </thead>
@@ -16,8 +18,15 @@
         @foreach($chamadas as $chamada)
         <tr>
             <td>{{ $chamada->id }}</td>
-            <td>{{ $chamada->title }}</td>
+            <td>{{ $chamada->data_e_hora_da_ligacao }}</td>
+            <td>{{ $chamada->numero_de_origem }}</td>
+            <td>{{ $chamada->numero_de_destino }}</td>
             <td class="text-right">
+                <form action="{{ route('chamadas.destroy', $chamada->id) }}" class="form-horizontal" method="post" style="display: inline-block">
+                    {!! csrf_field() !!}
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="submit" value="Excluir" class="btn btn-default btn-xs">
+                </form>
                 <a href="{{ route('chamadas.show', $chamada->id) }}" class="btn btn-default btn-xs">
                     <span class="glyphicon glyphicon-plus"></span>
                 </a>
