7e775d0c58dab3d4783214cec5f2d52c34b86903
João Moita Manrique <joaomoita@pacto.goiania>
2018-09-18 09:00:39 -0300

    adicionado SoftDeletes no model de chamada

--- app/Models/Chamada.php
+++ app/Models/Chamada.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Chamada extends Model
 {
+	use SoftDeletes;
+
     protected $fillable = [
     	'numero_de_origem', 'numero_de_destino', 'tempo_da_ligacao', 'data_e_hora_da_ligacao', 'comentario',
     ];
+    
+    protected $dates = ['deleted_at'];
 }
